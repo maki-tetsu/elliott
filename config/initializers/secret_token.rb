5553cc693a661ebbf5b921149dffa6e74000fc43
Tetsuhisa MAKINO <tim.makino@gmail.com>
2011-11-04 19:33:16 +0900

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elliott::Application.config.secret_token = '05e3e2189793fc1ab09815b8113fff3cc1bf434127719e4682e8b52a5d144216fabaa04161ce3c84e68186b90b550824fc9574de29bb0e4f895f457a98dcf404'
