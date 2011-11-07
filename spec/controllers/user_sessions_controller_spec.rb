0b01b932437ddcb1b51b9ad4b7e32de656d63a08
Tetsuhisa MAKINO <tim.makino@gmail.com>
2011-11-07 21:28:59 +0800

    ユーザ認証に関する AuthLogic の実装追加とテストコード実装

--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -1,5 +1,15 @@
+# -*- coding: utf-8 -*-
 require 'spec_helper'
 
 describe UserSessionsController do
+  describe "GET 'new'" do
+    context "ユーザがログインしていないとき" do
+      before(:each) do
+        get "new"
+      end
+    end
 
+    context "ユーザがログインしているとき" do
+    end
+  end
 end
